2b31aef738f1788f5880028b54f61a5b76810a48
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2024-01-19 20:27:33 +0000

    Update Filosofos.java

--- src/Filosofos.java
+++ src/Filosofos.java
@@ -1,174 +1,80 @@
-import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Filosofos {
-    public class Garfos extends Thread {
-        private int garfoEsquerda;
-        private int garfoDireita;
+    public class Garfos {
+        private Lock garfoEsquerda;
+        private Lock garfoDireita;
 
-        public  Garfos(int garfoEsquerda, int garfoDireita) {
-            this.garfoEsquerda = garfoEsquerda;
-            this.garfoDireita = garfoDireita;
+        public Garfos() {
+            this.garfoEsquerda = new ReentrantLock();
+            this.garfoDireita = new ReentrantLock();
         }
 
-        @Override
-        public void run() {
-            while (true){
-                try {
-                    SorteioDireita();
-                    SorteioEsquerda();
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex){
-                    System.out.println(ex);
-                }
-            }
+        public Lock getGarfoEsquerda() {
+            return garfoEsquerda;
         }
 
-        public void SorteioDireita() {
-            int garfoDireita;
-            Random d = new Random();
-            garfoDireita = d.nextInt(5);
-
-            System.out.println("O garfo da direita sorteado " + garfoDireita);
+        public Lock getGarfoDireita() {
+            return garfoDireita;
         }
-
-        public void SorteioEsquerda() {
-            int garfoEsquerda;
-            Random e = new Random();
-            garfoEsquerda = e.nextInt(5);
-
-            System.out.println("O garfo da esquerda sorteado " + garfoEsquerda);
-        }
-
     }
 
     public class Filosofo extends Thread {
-
         String nome;
-        Garfos garfo;
+        Garfos garfos;
         int lugarNaMesa;
 
-        public Filosofo(String nome, Garfos garfo, int lugarNaMesa) {
+        public Filosofo(String nome, Garfos garfos, int lugarNaMesa) {
             this.nome = nome;
-            this.garfo = garfo;
+            this.garfos = garfos;
             this.lugarNaMesa = lugarNaMesa;
 
-            System.out.println("O filosofo " + nome + " sentou a mesa.");
+            System.out.println("O filosofo " + nome + " sentou à mesa.");
         }
 
-        public  void Pensar() throws InterruptedException {
-            System.out.println("O filosofo " + nome + " esta pensando.");
+        public void pensar() throws InterruptedException {
+            System.out.println("O filosofo " + nome + " está pensando.");
+            Thread.sleep(1000);
+        }
+
+        public void comer() throws InterruptedException {
+            System.out.println("O filosofo " + nome + " está comendo.");
             Thread.sleep(1000);
         }
 
         @Override
         public void run() {
-            while(true){
-                if(garfo.garfoDireita == 5) {
-                    System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoDireita);
-                    if(garfo.garfoEsquerda == 1){
-                        System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoEsquerda);
-                        System.out.println("O Filosofo " + nome + " esta comendo.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da direita.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da esquerda.");
-                    } else {
-                        System.out.println("O Filosofo " + nome + " largou o garfo " + garfo.garfoDireita);
-                    }
+            while (true) {
+                try {
+                    pensar();
 
-                }else {
-                    try {
-                        pensar();
-                    } catch (InterruptedException ex) {
-                        System.out.println(ex);
-                    }
-                }
-                if(garfo.garfoDireita ==1){
-                    System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoDireita);
-                    if (garfo.garfoEsquerda == 2) {
-                        System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoEsquerda);
-                        System.out.println("O Filosofo " + nome + " esta comendo.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da direita.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da esquerda.");
-                    } else {
-                        System.out.println("O Filosofo " + nome + " Largou o garfo " + garfo.garfoDireita);
-                    }
-                } else {
-                    try {
-                        pensar();
-                    } catch (InterruptedException ex) {
-                        System.out.println(ex);
-                    }
-                }
+                    garfos.getGarfoEsquerda().lock();
+                    garfos.getGarfoDireita().lock();
 
-                if(garfo.garfoDireita == 2){
-                    System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoEsquerda);
-                    if(garfo.garfoEsquerda == 3) {
-                        System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoEsquerda);
-                        System.out.println("O Filosofo " + nome + " esta comendo.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da direita.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da esquerda.");
-                    } else {
-                        System.out.println("O Filosofo " + nome + " Largou o garfo " + garfo.garfoDireita);
-                    }
-                } else {
-                    try {
-                        pensar();
-                    } catch (InterruptedException ex) {
-                        System.out.println(ex);
-                    }
-                }
-                if(garfo.garfoDireita == 3){
-                    System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoEsquerda);
-                    if(garfo.garfoEsquerda == 4) {
-                        System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoEsquerda);
-                        System.out.println("O Filosofo " + nome + " esta comendo.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da direita.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da esquerda.");
-                    } else {
-                        System.out.println("O Filosofo " + nome + " Largou o garfo " + garfo.garfoDireita);
-                    }
-                } else {
-                    try {
-                        pensar();
-                    } catch (InterruptedException ex) {
-                        System.out.println(ex);
-                    }
-                }
-                if(garfo.garfoDireita == 4){
-                    System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoEsquerda);
-                    if(garfo.garfoEsquerda == 5) {
-                        System.out.println("O Filosofo " + nome + " pegou o garfo " + garfo.garfoEsquerda);
-                        System.out.println("O Filosofo " + nome + " esta comendo.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da direita.");
-                        System.out.println("O Filosofo " + nome + " largou o garfo da esquerda.");
-                    } else {
-                        System.out.println("O Filosofo " + nome + " Largou o garfo " + garfo.garfoDireita);
-                    }
-                } else {
-                    try {
-                        pensar();
-                    } catch (InterruptedException ex) {
-                        System.out.println(ex);
-                    }
+                    System.out.println("O Filosofo " + nome + " pegou os garfos.");
+
+                    comer();
+
+                    System.out.println("O Filosofo " + nome + " largou os garfos.");
+                } catch (InterruptedException ex) {
+                    System.out.println(ex);
+                } finally {
+                    garfos.getGarfoDireita().unlock();
+                    garfos.getGarfoEsquerda().unlock();
                 }
             }
         }
     }
 
-    public void test(){
+    public void test() {
+        Garfos garfos = new Garfos();
 
-        Garfos g1 = new Garfos(1, 5);
-        Garfos g2 = new Garfos(2, 1);
-        Garfos g3 = new Garfos(3, 3);
-        Garfos g4 = new Garfos(4, 3);
-        Garfos g5 = new Garfos(5, 4);
-
-        Filosofo filosofo1 = new Filosofo("Platao ", g1, 1);
-        Filosofo filosofo2 = new Filosofo("Aristoteles ", g2, 2);
-        Filosofo filosofo3 = new Filosofo("Socrates ", g3, 3);
-        Filosofo filosofo4 = new Filosofo("Descartes ", g4, 4);
-        Filosofo filosofo5 = new Filosofo("Euclides ", g5, 5);
+        Filosofo filosofo1 = new Filosofo("Platão", garfos, 1);
+        Filosofo filosofo2 = new Filosofo("Aristóteles", garfos, 2);
+        Filosofo filosofo3 = new Filosofo("Sócrates", garfos, 3);
+        Filosofo filosofo4 = new Filosofo("Descartes", garfos, 4);
+        Filosofo filosofo5 = new Filosofo("Euclides", garfos, 5);
 
         new Thread(filosofo1).start();
         new Thread(filosofo2).start();
@@ -177,3 +83,23 @@ public class Filosofos {
         new Thread(filosofo5).start();
     }
 }
+/*
+ A principal característica do ReentrantLock é que ele permite que um thread que já possui o bloqueio (lock) o
+ adquira novamente, sem ficar bloqueado, desde que o número de aquisições seja igual ao número de liberações.
+ Isso é chamado de "lock reentrancy" (reentrância de bloqueio).
+ Cada chamada lock precisa ser correspondida por uma chamada subsequente de unlock para liberar o bloqueio.
+
+    Reentrância:
+        Um thread pode chamar repetidamente lock em um ReentrantLock sem ficar bloqueado.
+        Ele precisa liberar o lock a mesma quantidade de vezes chamando unlock antes que outros threads possam adquiri-lo.
+
+    Métodos Principais:
+        lock(): Adquire o bloqueio. Se o bloqueio já estiver sendo mantido por outro thread,
+                o thread atual ficará bloqueado até que o bloqueio seja liberado.
+        unlock(): Libera o bloqueio. Deve ser chamado para corresponder a cada chamada lock.
+
+    Construtores e Configurações:
+        O construtor padrão cria um ReentrantLock com reentrância de bloqueio.
+        Existem construtores adicionais que permitem configurar o ReentrantLock para operar como um lock "fair" (justo),
+        onde os threads adquirem o bloqueio em ordem de chegada.
+ */
